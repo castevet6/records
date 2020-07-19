30247a04b617e7c2162eec9ce6bf2768183f7060
castevet6 <antti.e.manninen@tuni.fi>
2020-07-19 21:41:03 +0300

    Moved sandbox testing code to untracked file

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-    "records/models"
     "fmt"
 )
 
 func main() {
     fmt.Println("I'm alive")
-    var rec models.Record
-    rec.Id = 1
-    rec.Author = "Satanic Warmaster"
-    rec.Title = "Opferblut"
-    rec.Format = "LP"
-    rec.Genre = "Black Metal"
-    rec.Category = "Vinyl"
-    rec.Publisher = "No Colours Records"
-    rec.Year = 2003
-
-    fmt.Println(rec.ToString())
 }
