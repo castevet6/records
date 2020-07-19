33cea3ef8c86a513be68c9fafae28cbaf7820701
castevet6 <antti.e.manninen@tuni.fi>
2020-07-19 21:37:41 +0300

    Record model + model ToString() implemented

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-    models "records/models"
+    "records/models"
     "fmt"
 )
 
 func main() {
     fmt.Println("I'm alive")
-    r := models.Record{id:1, author:"Satanic Warmaster"}
-    /* r.id = 1
-    r.author = "Satanic Warmaster"
-    r.title = "Opferblut"
-    r.format = "LP"
-    r.category = "Vinyl"
-    r.genre = "Black Metal"
-    r.publisher = "No Colours Records"
-    r.year = 2003*/
+    var rec models.Record
+    rec.Id = 1
+    rec.Author = "Satanic Warmaster"
+    rec.Title = "Opferblut"
+    rec.Format = "LP"
+    rec.Genre = "Black Metal"
+    rec.Category = "Vinyl"
+    rec.Publisher = "No Colours Records"
+    rec.Year = 2003
+
+    fmt.Println(rec.ToString())
 }
--- models/record.go
+++ models/record.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Record struct {
-    id          int     `json:"id"`
-    author      string  `json:"author"`
-    title       string  `json:"title"`
-    format      string  `json:"format"`
-    category    string  `json:"category"`
-    genre       string  `json:"genre"`
-    publisher   string  `json:"publisher"`
-    year        int     `json:"year"`
+    Id          int     `json:"id"`
+    Author      string  `json:"author"`
+    Title       string  `json:"title"`
+    Format      string  `json:"format"`
+    Category    string  `json:"category"`
+    Genre       string  `json:"genre"`
+    Publisher   string  `json:"publisher"`
+    Year        int     `json:"year"`
 }
 
-func (r *Record) toString() string {
-    return fmt.Sprintf("%s: %s %s (%s, %d)", s.ToUpper(r.author), r.title, r.format, r.publisher, r.year)
+func (r *Record) ToString() string {
+    return fmt.Sprintf("%s: %s %s (%s, %d)", s.ToUpper(r.Author), r.Title, r.Format, r.Publisher, r.Year)
 }
